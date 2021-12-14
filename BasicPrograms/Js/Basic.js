ea69205cefa5a422cb6605e5cc816ca893b2d165
Mabel Christina <maybelchristina@gmail.com>
2021-12-14 21:02:34 +0530

    [Mabel] Add: js html and css added

---
+++ BasicPrograms/Js/Basic.js
@@ -0,0 +1,6 @@
+console.log('hello');
+
+function add(a,b){
+    return a+b;
+}
+console.log('the addition is',add(10,6));
---
+++ BasicPrograms/Js/Calculator.js
@@ -0,0 +1,15 @@
+function display(val)
+{
+ document.getElementById("textval").value+=val
+ }
+function evaluate()
+{
+ let x = document.getElementById("textval").value
+ let y = eval(x)
+ document.getElementById("textval").value = x
+ document.getElementById("textval").value = y
+}
+function clr()
+{
+ document.getElementById("textval").value = ""
+}
